96755951175dec7bbe79a52b4cb6209da2875c82
ciberSi4 <alex.baikonur@mail.ru>
2024-08-22 19:37:07 +0300

    Add files via upload

---
+++ dynamic-typing.py
@@ -0,0 +1,9 @@
+# Динамическая типизация
+name = "Алексей"
+print ( "name:", name )
+age = 50
+print ( "age:", age )
+age += 1                        # age = age + 1
+print ( "age+:", age )
+is_student = True
+print ( "is_student:", is_student )
